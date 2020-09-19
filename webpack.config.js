5587b59cad2df5632651119aaea972a998e4b801
Kubilay Caglayan <kblycaglayan@gmail.com>
2020-09-19 23:50:11 +0300

    Add dependency wepack server

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  devServer: {
+    contentBase: './dist',
+  },
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
