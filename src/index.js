898f5e8c4d2df6c504195079dade6eb7cf03fc7a
Kubilay Caglayan <kblycaglayan@gmail.com>
2020-09-20 00:11:00 +0300

    Add babel-loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 const helloWorld = document.createElement('h1');
-helloWorld.innerText = 'Hello Webpack!';
+helloWorld.innerText = 'Hello Webpack!!!';
 
 document.getElementsByTagName('body')[0].appendChild(helloWorld);
